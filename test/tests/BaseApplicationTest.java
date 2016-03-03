a829f948abcf99bba6fd708bf8ef880a770af12d
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-03-03 19:24:25 +0100

    Some comments and small improvements

--- test/tests/BaseApplicationTest.java
+++ test/tests/BaseApplicationTest.java
@@ -13,9 +13,12 @@ public class BaseApplicationTest {
 
 	@Test
 	public final void test() {
+		
+		// DD Create a type declaring attributes
 		Type person = new Type("Person", new String[] {"name", "email", "employer"});
 		Type company = new Type("Company", new String[] {"name", "url" });
 		
+		// DM Create an instance of this type, set and get attribute values
 		Instance acme = company.instanciate();
 		acme.setAttributeValue("name", "Acme INc.");
 		acme.setAttributeValue("url", "http://www.acme.co.uk");
