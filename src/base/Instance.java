00a2ee22a3b866c62733c5d8b1cdb6436206ddb5
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-01 14:10:23 +0200

    Fix whitespace

--- src/base/Instance.java
+++ src/base/Instance.java
@@ -3,7 +3,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Instance {
-	
+
 	private final Type type;
 	private final Map<String, Object> dictAttributes;
 
@@ -11,23 +11,23 @@ public class Instance {
 		this.dictAttributes = new HashMap<String, Object>();
 		this.type = type;
 	}
-	
+
 	public void setAttributeValue(String attrName, Object attrValue) {
 		type.exceptWhenAttributeNotAvailable(attrName);
 		this.dictAttributes.put(attrName, attrValue);
 	}
-	
+
 	public Object getAttributeValue(String attrName) {
-		
+
 		type.exceptWhenAttributeNotAvailable(attrName);
-		
+
 		return this.dictAttributes.get(attrName);
 	}
 
 	public void invoke(String methodName) {
-		type.invoke(methodName);	
+		type.invoke(methodName);
 	}
-	
+
 	public String getTypeName() {
 		return type.getName();
 	}
